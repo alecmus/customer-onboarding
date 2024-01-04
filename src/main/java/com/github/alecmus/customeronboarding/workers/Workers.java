adce8b6ac6817cdc5ea54e3349c998ff87f1002c
Alec Musasa <alecmus@live.com>
2024-01-04 20:44:04 +0200

    Using constructor injection in Workers

--- src/main/java/com/github/alecmus/customeronboarding/workers/Workers.java
+++ src/main/java/com/github/alecmus/customeronboarding/workers/Workers.java
@@ -3,6 +3,8 @@ package com.github.alecmus.customeronboarding.workers;
 import io.camunda.zeebe.client.ZeebeClient;
 import io.camunda.zeebe.client.api.worker.JobWorker;
 import jakarta.annotation.PostConstruct;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
@@ -12,14 +14,21 @@ import java.time.Duration;
 @Component
 public class Workers {
 
-    @Autowired
+    private static Logger logger = LoggerFactory.getLogger(Workers.class);
+
     private ZeebeClient client;
+    private RestTemplate restTemplate;
 
     @Autowired
-    private RestTemplate restTemplate;
+    public Workers(ZeebeClient client, RestTemplate restTemplate) {
+        this.client = client;
+        this.restTemplate = restTemplate;
+    }
 
     @PostConstruct
     public void createWorkers() {
+        logger.info("Creating workers");
+
         JobWorker scoreCustomerWorker = client.newWorker()
                 .jobType("scoreCustomer")
                 .handler(new ScoreCustomerHandler())
@@ -31,5 +40,7 @@ public class Workers {
                 .handler(new AddCustomerToCrmHandler(restTemplate))
                 .timeout(Duration.ofSeconds(10).toMillis())
                 .open();
+
+        logger.info("Done creating workers");
     }
 }
