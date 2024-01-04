e0e95284294c9d70f4fc1373f8fbb900b45d1a61
Alec Musasa <alecmus@live.com>
2024-01-04 13:00:59 +0200

    Added deployment annotation

--- src/main/java/com/github/alecmus/customeronboarding/CustomerOnboardingApplication.java
+++ src/main/java/com/github/alecmus/customeronboarding/CustomerOnboardingApplication.java
@@ -1,8 +1,11 @@
 package com.github.alecmus.customeronboarding;
 
+import io.camunda.zeebe.spring.client.annotation.Deployment;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// attempts deploying the diagram on startup using zeebe.client.cloud creds in properties
+@Deployment(resources = "classpath:customer-onboarding.bpmn")
 @SpringBootApplication
 public class CustomerOnboardingApplication {
 
